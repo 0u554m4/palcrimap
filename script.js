310eec9ee76ce35b06896888147abd79a7bba8cf
0u554m4 <0u55x4m4@gmail.com>
2026-02-23 01:28:26 +0100

    change map

--- script.js
+++ script.js
@@ -730,9 +730,9 @@ document.addEventListener('DOMContentLoaded', function() {
         
     });
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; OpenStreetMap, CartoDB'
-    }).addTo(window.map);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; CartoDB'
+}).addTo(window.map);
 
     // Store markers globally for language updates
     window.crimeMarkers = [];
